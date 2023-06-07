fa7d8f85c06d5d11a1a1b9db24e1ac2762d4817d
lucienMasn <lucien.masn@gmail.com>
2023-06-07 09:49:26 +0200

    Commit Git 07/06/23

--- APP_FILMS_164/genres/gestion_genres_wtf_forms.py
+++ APP_FILMS_164/genres/gestion_genres_wtf_forms.py
@@ -64,7 +64,7 @@ class FormWTFDeleteGenre(FlaskForm):
         submit_btn_conf_del : Bouton de confirmation pour effacer un "genre".
         submit_btn_annuler : Bouton qui permet d'afficher la table "t_genre".
     """
-    nom_genre_delete_wtf = StringField("Effacer ce genre")
-    submit_btn_del = SubmitField("Effacer genre")
+    nom_genre_delete_wtf = StringField("Effacer cette personne")
+    submit_btn_del = SubmitField("Effacer personne")
     submit_btn_conf_del = SubmitField("Etes-vous sur d'effacer ?")
     submit_btn_annuler = SubmitField("Annuler")
